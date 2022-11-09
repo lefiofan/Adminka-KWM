1933cf840287872d98f15fa2c4d64fa7b0fca7a5
Lefiofan <lefiofan21@gmail.com>
2022-11-09 06:43:34 +0600

    Добавил докер

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['*', 'localhost:8989']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
